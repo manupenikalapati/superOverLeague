a64d202df4d680257ea1555320b2059b0216fad2
Manmohan Chowdary <manmohanchowdary510@gmail.com>
2023-12-08 06:41:14 +0000

    superOverLeagueCode

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-color">
+    <div>
+      <h1 className="heading-style">Super Over League</h1>
+      <div className="subCard">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="imgStyle"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+          className="imgStyle"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
